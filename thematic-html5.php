07cf4e061833e874260274b161e805a476b6ec1a
Middlesister <invistruct@gmail.com>
2012-02-09 11:40:11 +0100

    First commit, add plugin file and languages folder

---
+++ thematic-html5.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Thematic HTML5
+ * Plugin URI: http://github.com/middlesister/thematic-html5
+ * Description: A wordpress plugin to use html5 markup with the thematic theme framework 
+ * Version: 0.1
+ * Author: Karin Taliga a.k.a Middlesister
+ * 
+ * @package Thematic-HTML5
+ */
+
+/* Launch the plugin. */
+add_action( 'plugins_loaded', 'thematic_html5_init' );
+
+/**
+ * Initialize the plugin
+ *
+ * @since 0.1
+ **/
+function thematic_html5_init() {
+	/* Load the translation of the plugin. */
+	load_plugin_textdomain( 'thm_html5', false, '/languages' );
+	
+	
+}
+
+
+
+?>
