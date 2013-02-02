627fc7735a9a1381f5f4c357c749e8b0a57ae9fe
Middlesister <invistruct@gmail.com>
2013-02-02 23:38:36 +0100

    Add boilerplate classes to doctype declaration

--- class-ivst-thematic-html5.php
+++ class-ivst-thematic-html5.php
@@ -186,12 +186,17 @@ class Ivst_Thematic_Html5 {
 	/**
 	 * Create the html5 doctype instead of xhtml1.
 	 * 
-	 * @since 0.1 
+	 * Uses classes from html5boilerplate @link{http://html5boilerplate.com}
+	 * 
+	 * @since 0.4 
 	 */
 	function create_doctype( $content ) {
-		$content = '<!DOCTYPE html>';
-		$content .= "\n";
-		$content .= '<html';
+		$content = '<!doctype html>' . "\n";
+		$content .= '<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" dir="' . get_bloginfo ('text_direction') . '" lang="'. get_bloginfo ('language') . '"> <![endif]-->' . "\n";
+		$content .= '<!--[if IE 7]> <html class="no-js lt-ie9 lt-ie8" dir="' . get_bloginfo ('text_direction') . '" lang="'. get_bloginfo ('language') . '"> <![endif]-->'. "\n";
+		$content .= '<!--[if IE 8]> <html class="no-js lt-ie9" dir="' . get_bloginfo ('text_direction') . '" lang="'. get_bloginfo ('language') . '"> <![endif]-->' . "\n";
+		$content .= '<!--[if gt IE 8]><!-->' . "\n";
+		$content .= '<html class="no-js" dir="' . get_bloginfo ('text_direction') . '"';
 		return $content;
 	}
 
@@ -202,7 +207,8 @@ class Ivst_Thematic_Html5 {
 	 * @since 0.1 
 	 */
 	function head_profile( $content ) {
-		$content = '<head>';
+		$content = '<!--<![endif]-->' . "\n";
+		$content .= '<head>';
 		$content .= "\n";
 		$content .= '<meta charset="' . get_bloginfo( 'charset' ) . '">';
 		$content .= "\n";
